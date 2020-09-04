f843e4c0b09471e7b95420c6b1f9bd637d893005
SeerLite <seerlite@tuta.io>
2020-09-04 19:50:02 -0400

    Lots of changes im lazy

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD_ID = 691468513239367761
 VOICE_CHANNEL = "Among Us"
 TEXT_CHANNEL = "litebot"
-EXCLUDE_ROLE = "Music Bots"
+EXCLUDE_ROLE = "Music Botss"
 
 bot = commands.Bot(command_prefix="lt:")
 
@@ -16,9 +16,15 @@ bot.among_us_vc = None
 bot.litebot_channel = None
 
 bot.mimic = None
+bot.is_muting = False
 bot.tracked_members = []
-bot.managed_mutes = []
 bot.control_panel = None
+
+class Tracked_member():
+    def __init__(self, member, is_visible=True, is_dead=False):
+        self.member = member
+        self.is_visible = is_visible
+        self.is_dead = is_dead
 
 @bot.event
 async def on_ready():
@@ -28,8 +34,19 @@ async def on_ready():
     bot.among_us_vc = discord.utils.get(bot.managed_guild.voice_channels, name=VOICE_CHANNEL)
     bot.tracked_members = bot.among_us_vc.members
     bot.tracked_members = list(filter(lambda m: not discord.utils.get(m.roles, name=EXCLUDE_ROLE), bot.tracked_members))
-    bot.tracked_members = [dict(member=member, is_visible=True) for member in bot.tracked_members] # convert to dicts
+    bot.tracked_members = [Tracked_member(member) for member in bot.tracked_members] # convert to dicts
     await send_control_panel()
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user or message.channel != bot.litebot_channel:
+        return
+    if message.content.isdigit():
+        index = int(message.content) - 1
+        if index in range(len(bot.tracked_members)):
+            await message.delete()
+            bot.tracked_members[index].is_dead = not bot.tracked_members[index].is_dead
+            await set_mute(bot.is_muting)
 
 async def send_control_panel():
     if bot.control_panel:
@@ -41,46 +58,38 @@ async def send_control_panel():
 
 async def update_control_panel():
     if bot.control_panel:
-        member_list = "\n".join([str(bot.tracked_members.index(member_dict) + 1) + ": " + member_dict["member"].display_name for member_dict in filter(lambda m: m["is_visible"], bot.tracked_members)])
+        member_list = "\n".join([str(bot.tracked_members.index(tracked_member) + 1) + ": " + tracked_member.member.display_name + (" (DEAD)" if tracked_member.is_dead else "") for tracked_member in filter(lambda m: m.is_visible, bot.tracked_members)])
         if bot.mimic:
             mimic_text = f"Mimicking: {bot.mimic.display_name}"
         else:
             mimic_text = "Not mimicking anyone! React with :copyright: to mimic you!"
-        await bot.control_panel.edit(content=f"Members in Among Us:\n```{member_list}\n```{mimic_text}")
+        await bot.control_panel.edit(content=f"**Muting:** {'Yes' if bot.is_muting else 'No'}\nMembers in Among Us:\n```{member_list}\n```{mimic_text}")
 
-async def set_mute(mute_state, check_managed=True):
-    members = bot.among_us_vc.members
-    if bot.mimic:
-        members.remove(bot.mimic) # filter out mimicked member
-    members = list(filter(lambda m: not discord.utils.get(m.roles, name=EXCLUDE_ROLE), members)) # filter out Music Bots
-    members = list(filter(lambda m: m.voice.mute != mute_state, members)) # filter out users already server-muted/unmuted
-    if mute_state:
-        bot.managed_mutes += members
-    if not mute_state and check_managed:
-        members = list(filter(lambda m: m in bot.managed_mutes, members))
-        bot.managed_mutes = []
-    await asyncio.gather(*(member.edit(mute=mute_state) for member in members))
-    return members
-
-@bot.command(name="forceunmute", help=f"Force unmutes everyone in {VOICE_CHANNEL}")
-async def forceunmute(ctx):
-    muted_members = await set_mute(False, False)
-    await ctx.send(f"Force unmuted everyone:\n{' - '.join((member.display_name for member in muted_members))}")
-
-@bot.command(name="listmanaged", help="Lists managed mutes")
-async def listmanaged(ctx):
-    await ctx.send(f"Server muted members managed by LiteBot:\n{' - '.join((member.display_name for member in bot.managed_mutes))}")
+async def set_mute(mute_state):
+    bot.is_muting = mute_state
+    await update_control_panel()
+    tasks = []
+    for tracked_member in bot.tracked_members:
+        if tracked_member.is_visible: # in vc
+            if tracked_member.is_dead:
+                tasks.append(tracked_member.member.edit(mute=True, deafen=False))
+            elif tracked_member.member == bot.mimic:
+                tasks.append(tracked_member.member.edit(mute=mute_state, deafen=False))
+            else:
+                tasks.append(tracked_member.member.edit(mute=mute_state, deafen=mute_state))
+    await asyncio.gather(*tasks)
 
 # Raw mimic function
 async def set_mimic(member):
     if member:
         if member.voice and member.voice.channel == bot.among_us_vc:
             bot.mimic = member
-            await update_control_panel()
+            await set_mute(bot.is_muting)
             return True
         else:
             return False
     else:
+        # TODO: different behavior depending on if user left or just stopped mimicking.
         bot.mimic = None
         await update_control_panel()
 
@@ -90,34 +99,32 @@ async def on_voice_state_update(member, before, after):
         return
     if member == bot.mimic:
         if after.channel == bot.among_us_vc: # Status changed inside channel
-            if before.self_mute != after.self_mute:
-                if after.self_mute:
-                    await set_mute(True)
-                else:
-                    await set_mute(False)
+            if before.self_deaf != after.self_deaf:
+                await set_mute(after.self_deaf)
         else:                                # Whoops, not in channel anymore?
             await set_mimic(None)
     if after.channel == bot.among_us_vc:
-        if not member in [member_dict["member"] for member_dict in bot.tracked_members]:
-            bot.tracked_members.append(dict(member=member, is_visible=True))
+        if not member in [tracked_member.member for tracked_member in bot.tracked_members]:
+            bot.tracked_members.append(Tracked_member(member))
+            await update_control_panel()
         else:
-            for member_dict in bot.tracked_members:
-                if member == member_dict["member"]:
-                    member_dict["is_visible"] = True
-    elif after.channel != bot.among_us_vc and member in [member_dict["member"] for member_dict in bot.tracked_members]:
-        for member_dict in bot.tracked_members:
-            if member == member_dict["member"]:
-                member_dict["is_visible"] = False
-    await update_control_panel()
+            for tracked_member in bot.tracked_members:
+                if member == tracked_member.member and not tracked_member.is_visible:
+                    tracked_member.is_visible = True
+                    await update_control_panel()
+    elif after.channel != bot.among_us_vc and member in [tracked_member.member for tracked_member in bot.tracked_members]:
+        for tracked_member in bot.tracked_members:
+            if member == tracked_member.member and tracked_member.is_visible:
+                tracked_member.is_visible = False
+                await update_control_panel()
 
 @bot.event
 async def on_reaction_add(reaction, member):
     if member == bot.user:
         return
-    await reaction.message.remove_reaction(reaction.emoji, member)
     if reaction.message.id == bot.control_panel.id:
         if reaction.emoji == '🔈':
-            if len(bot.managed_mutes):
+            if bot.is_muting:
                 await set_mute(False)
             else:
                 await set_mute(True)
@@ -126,6 +133,7 @@ async def on_reaction_add(reaction, member):
                 await set_mimic(member)
             elif member == bot.mimic:
                 await set_mimic(None)
+    await reaction.remove(member)
 
 bot.run(TOKEN)
 
